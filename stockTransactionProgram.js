40044f0620a554ae976b63062dbc536937bb4d0f
Alex Abushady <aabushady214@gmail.com>
2019-08-28 09:03:14 -0400

    Update stockTransactionProgram.js
    Added comments that display the objective of the program.

--- stockTransactionProgram.js
+++ stockTransactionProgram.js
@@ -1,3 +1,13 @@
+/*
+* The program should display the following information...
+* The amount of money Tara paid for the stock: $40.00
+* The amount of comission Tara paid her broker when she bought the stock: 3% of total purchase.
+* The amount that Tara sold the stock for: $42.75
+* The amount of commission Tara paid her broker when she sold: 3% the amount recieved from the stocks.
+* The amount of money that Tara had when she sold and paid her broker(both times).l IF the amount is positve,
+* then display "Tara made a profit!". If the amount is negative, display "Tara lost money...".
+*/
+
 (function () {
     'use strict';
 
