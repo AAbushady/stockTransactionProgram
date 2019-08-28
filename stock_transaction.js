45f844dcc2de64e70efeb63cf530d9f934b8bac8
Alex Abushady <aabushady214@gmail.com>
2019-08-28 09:31:28 -0400

    Update stock_transaction.js
    Added Pseudocode section and repurposed old comment section for a README file...

--- stock_transaction.js
+++ stock_transaction.js
@@ -1,33 +1,43 @@
+// Pseudocode
 /*
-* The program should display the following information...
-* The amount of money Tara paid for the stock: $40.00
-*
-* The amount of comission Tara paid her broker when she bought the stock: 3% of total purchase.
-*
-* The amount that Tara sold the stock for: $42.75
-* 
-* The amount of commission Tara paid her broker when she sold: 3% the amount recieved from the stocks.
-* 
-* The amount of money that Tara had when she sold and paid her broker(both times). If the amount is positve,
-*   then display "Tara made a profit!". If the amount is negative, display "Tara lost money...".
-*
-* amountPaid = 2000 * 40.00
-* Tara paid $80,000 for the stock.
-* 
-* comissionPaid = 80000 * .03
-* Tara paid her broker $2,400.
-*
-* saleAmount = 2000 * 42.75
-* Tara sold for $85,500.
-*
-* comissionPaid = 85500 * .03
-* Tara paid her broker another $2,565
-*
-* 85500 - 80000 = 5500
-* 2400 + 2565 = 4965
-* 5500 - 4965 = 535
-* 535 > 0 == true
-* Tara did make a profit.
+    The amount of money Tara paid for stocks...
+
+    Amount Paid = $ 80,000
+    2000 * 40.00 = 80000.00
+    amountPaid = sharesPurchased * purchasePrice
+    
+    The amount of comission Tara paid her broker...
+    
+    comission paid = $2,400
+    80000 * 0.3 = 2400.00
+    comissionPaidOnPurchase = amountPaid * comissionRate
+    
+    The amount that Tara sold the stock for...
+    
+    sale amount = $85,500
+    2000 * 42.75 = 85500
+    saleAmount = sharesSold * salePrice
+    
+    The amount of commission Tara paid her broker when she sold stock...
+    
+    comission paid = $2,565
+    85500 * 0.3 = 2565.00
+    comissionPaidOnSale = saleAmount * comissionRate
+    
+    The amount of money leftover after Tara sells her stock, which if greater than 0 means Tara made a profit...
+    
+    Tara made a profit
+    
+    85500 - 85500 = 5500
+    2400 + 2565 = 4965
+    5500 - 4965 = 535
+    535 > 0 so Tara made a profit
+    
+    profit = (saleAmoujnt - purchase Price) - (comissionPaidOnPurchase + comissionPaidOnSale)
+    if profit > 0
+        display Made Profit
+    else
+        display Lost Money
 */
 
 (function () {
