d5b5e512735969b3611fc70d8294784de5b117b5
Alex Abushady <aabushady214@gmail.com>
2019-10-08 11:44:53 -0400

    Update stock_transaction.js
    Cleaned up code, switched some vars to lets.

--- stock_transaction.js
+++ stock_transaction.js
@@ -1,72 +1,33 @@
-// Pseudocode
-/*
-    The amount of money Tara paid for stocks...
-
-    Amount Paid = $ 80,000
-    2000 * 40.00 = 80000.00
-    amountPaid = sharesPurchased * purchasePrice
-    
-    The amount of comission Tara paid her broker...
-    
-    comission paid = $2,400
-    80000 * 0.3 = 2400.00
-    comissionPaidOnPurchase = amountPaid * comissionRate
-    
-    The amount that Tara sold the stock for...
-    
-    sale amount = $85,500
-    2000 * 42.75 = 85500
-    saleAmount = sharesSold * salePrice
-    
-    The amount of commission Tara paid her broker when she sold stock...
-    
-    comission paid = $2,565
-    85500 * 0.3 = 2565.00
-    comissionPaidOnSale = saleAmount * comissionRate
-    
-    The amount of money leftover after Tara sells her stock, which if greater than 0 means Tara made a profit...
-    
-    Tara made a profit
-    
-    85500 - 85500 = 5500
-    2400 + 2565 = 4965
-    5500 - 4965 = 535
-    535 > 0 so Tara made a profit
-    
-    profit = (saleAmoujnt - purchase Price) - (comissionPaidOnPurchase + comissionPaidOnSale)
-    if profit > 0
-        display Made Profit
-    else
-        display Lost Money
-*/
+/*global console*/
+/*jshint esversion:6*/
 
 (function () {
     'use strict';
 
     // Variables and Constants
-    var sharesPurchased = 2000;
-    var purchasePrice = 40.00;
-    let comissionRate = .03;
-    var sharesSold = 2000;
-    var salesPrice = 42.75;
-    
+    let sharesPurchased = 2000;
+    let purchasePrice = 40.00;
+    let comissionRate = 0.03;
+    let sharesSold = 2000;
+    let salesPrice = 42.75;
+
     // Calculate the numbers
     let amountPaid = sharesPurchased * purchasePrice;
     let comissionPaidOnPurchase = amountPaid * comissionRate;
     let saleAmount = sharesSold * salesPrice;
     let comissionPaidOnSale = saleAmount * comissionRate;
     let profit = (saleAmount - amountPaid) - (comissionPaidOnPurchase + comissionPaidOnSale);
-    
+
     // Display Results
     console.log(`Amount Paid: \$${amountPaid.toFixed(2)}`);
     console.log(`Comission Paid on Purchase: \$${comissionPaidOnPurchase.toFixed(2)}`);
     console.log(`Sale Amount: : \$${saleAmount.toFixed(2)}`);
     console.log(`Comission Paid on Sale: \$${comissionPaidOnSale.toFixed(2)}`);
     console.log(`Profit: \$${profit.toFixed(2)}`);
-    
+
     if (profit > 0.00) {
         console.log("Tara Made a Profit!!");
     } else {
-        console.log("Tara Lost Money...")
+        console.log("Tara Lost Money...");
     }
 })();
